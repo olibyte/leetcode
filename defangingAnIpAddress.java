21e19466c6fc7714a8d16662a3334457538f5c77
Oliver <ocben1@gmail.com>
2020-09-03 07:35:06 +1000

    defangingAnIpAddress

---
+++ defangingAnIpAddress.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        char[] ipCharacters = address.toCharArray();
+        StringBuffer defangedAddress = new StringBuffer();
+        for (int i = 0; i < ipCharacters.length; i++) {
+            if (ipCharacters[i] == '.') {
+                defangedAddress.append("[.]");
+            } else {
+                defangedAddress.append(ipCharacters[i]);
+            }
+        }
+        return defangedAddress.toString();
+
+        // 100% faster
+        // return address.replace(".", "[.]");
+    }
+}
