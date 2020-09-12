57dce2c7a7e705fe6cc9f03cbf70760fcd53430e
Oliver <ocben1@gmail.com>
2020-09-12 20:04:28 +1000

    todo create array

--- decompressRLElist.java
+++ decompressRLElist.java
@@ -1,5 +1,20 @@
 class Solution {
     public int[] decompressRLElist(int[] nums) {
-        
+        //iterate through nums, stepping 2 elements at a time to eval freq to calculate length of our soon to be decompressed list
+        int len = 0;
+        for (int i = 0; i < nums.length; i+=2) {
+            len += nums[i];
+        }
+        //initialize decompress list, with counter to ensure val is placed at proper index in list
+        int result[] = new int[len];
+        int k = 0;
+        for (int i = 0; i < nums.length; i+=2) {
+            //use k to append val to result freq number of times
+            for (int j = 0; j < nums[i]; j++) {
+                result[k] = nums[i+1];
+                k++;
+            }
+        }
+        return result;
     }
 }
