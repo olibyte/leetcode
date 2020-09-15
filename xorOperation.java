4af068b6c8c72900c825302afdd85d21b2da7f02
Oliver <ocben1@gmail.com>
2020-09-15 21:48:57 +1000

    bitwise xor

---
+++ xorOperation.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int[] nums = new int[n];
+        int bitwiseXOR  = 0;
+        for (int i = 0; i < n; i++) {
+            nums[i] = start + 2*i;
+        }
+        
+
+        return bitwiseXOR;
+    }
+}
