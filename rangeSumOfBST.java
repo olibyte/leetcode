1279b9072ef155f9a07acb0f12953718cfb0e949
Oliver <ocben1@gmail.com>
2020-09-18 09:06:06 +1000

    rangeSumofBST

--- rangeSumOfBST.java
+++ rangeSumOfBST.java
@@ -15,6 +15,25 @@
  */
 class Solution {
     public int rangeSumBST(TreeNode root, int L, int R) {
-        
+          if (root == null) {
+            return 0;
+        }
+        //BFS
+        int sum = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            TreeNode current = queue.remove();
+            if(current.val >= L && current.val <= R) {
+                sum+= current.val;
+            }
+            if(current.left != null && current.val > L) {
+                queue.add(current.left);
+            }
+            if(current.right != null && current.val < R) {
+                queue.add(current.right);
+            }
+        }
+        return sum;
     }
 }
