d81568f317407ced4bc72279a677484274c55967
Oliver <ocben1@gmail.com>
2020-09-23 07:28:50 +1000

    count good triplets

--- countGoodTriplets.java
+++ countGoodTriplets.java
@@ -1,7 +1,17 @@
 class Solution {
     public int countGoodTriplets(int[] arr, int a, int b, int c) {
-        int result = 0;
-
-        return result;
+        int tripletCount = 0;
+        // brute force
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                if (Math.abs(arr[i] - arr[j]) <= a)
+                    for (int k = j + 1; k < arr.length; k++) {
+                        if (Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) {
+                            tripletCount++;
+                        }
+                    }
+            }
+        }
+        return tripletCount;
     }
 }
