4d67ca78bd827e455d915d4b660ed7f705642e1f
Oliver <ocben1@gmail.com>
2020-10-06 07:55:35 +1100

    matrix diagonal sum

---
+++ matrixDiagonalSum.java
@@ -0,0 +1,17 @@
+public class matrixDiagonalSum {
+    public int diagonalSum(int[][] mat) {
+        
+        int sum = 0;
+        
+        for(int i = 0; i < mat.length / 2; i++) {
+            //from the top
+                sum += mat[i][i] + mat[i][mat.length-1-i];
+                //from the bottom
+                sum += mat[mat.length-1-i][i] + mat[mat.length-1-i][mat.length-1-i];
+        }
+        if (mat.length % 2 != 0)
+            sum += mat[mat.length/2][mat.length/2];
+        
+        return sum;
+    }
+}
