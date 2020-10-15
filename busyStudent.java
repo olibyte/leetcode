9dfecffd61d2547676f69654d279847e2e92608b
Oliver <ocben1@gmail.com>
2020-10-16 07:27:42 +1100

    busy student

--- busyStudent.java
+++ busyStudent.java
@@ -1,5 +1,13 @@
+import java.util.HashMap;
+
 public class busyStudent {
     public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
-        
+        int studentCount = 0;
+        for (int i = 0; i < startTime.length; i++) {
+            if (startTime[i] <= queryTime && queryTime <= endTime[i]) {
+                studentCount++;
+            }
+        }
+        return studentCount;
     }
 }
