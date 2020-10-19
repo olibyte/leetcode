5d82b381b1b57ce84bef793720203b6238ef60a5
Oliver <ocben1@gmail.com>
2020-10-20 06:38:32 +1100

    sumZero

--- sumZero.java
+++ sumZero.java
@@ -1,7 +1,12 @@
 public class sumZero {
     public int[] sumZero(int n) {
         int result[] = new int[n];
-
+        
+        for (int i = 0; i < n-1; i+=2) {
+            result[i] = i+1;
+            result[i+1] = -result[i];
+        }
+        //5 => [1,-1,3,-3,5]
         return result;
         
     }
