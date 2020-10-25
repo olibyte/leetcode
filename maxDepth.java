44fd6321704b8feacf4c0f945c7c7dfb951a108e
Oliver <ocben1@gmail.com>
2020-10-26 07:31:26 +1100

    maxDepth

--- maxDepth.java
+++ maxDepth.java
@@ -1,6 +1,5 @@
 public class maxDepth {
     public int maxDepth(String s) {
-        //!= "(" || != ")"
         int parenthesisCount = 0;
         int maxDepth = 0;
         for (int i = 0; i < s.length(); i++) {
