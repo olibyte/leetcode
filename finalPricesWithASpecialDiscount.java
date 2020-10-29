e16c5870f2413125380e36336147506433749bcd
Oliver <ocben1@gmail.com>
2020-10-30 07:20:07 +1100

    finalPrices

--- finalPricesWithASpecialDiscount.java
+++ finalPricesWithASpecialDiscount.java
@@ -1,7 +1,20 @@
 public class finalPricesWithASpecialDiscount {
     public int[] finalPrices(int[] prices) {
-        int[] result = new int[prices.length];
-        
+        int n = prices.length;
+        int[] result = new int[n];
+        for (int i = 0; i < n -1; i++) {
+           for (int j = i + 1; j < n; j++) {
+                if (prices[j] <= prices[i]) {
+                    result[i] = prices[i] - prices[j];
+                    prices[i] -= prices[j];
+                    break;
+                } 
+                else {
+                    result[i] = prices[i];
+                }
+            }
+        }
+        result[n-1] = prices[n-1];
         return result;
     }
 }
