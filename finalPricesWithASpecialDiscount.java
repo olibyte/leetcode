bbda989d445ae4db882128d0f5d94a8f1e9600b4
Oliver <ocben1@gmail.com>
2020-10-29 20:58:19 +1100

    final prices

---
+++ finalPricesWithASpecialDiscount.java
@@ -0,0 +1,7 @@
+public class finalPricesWithASpecialDiscount {
+    public int[] finalPrices(int[] prices) {
+        int[] result = new int[prices.length];
+        
+        return result;
+    }
+}
