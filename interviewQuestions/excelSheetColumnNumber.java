bedbd192e841e6759bb8d7fa60f64c01a2758578
Oliver <ocben1@gmail.com>
2020-11-16 08:11:21 +1100

    excel sheet column number

---
+++ interviewQuestions/excelSheetColumnNumber.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class excelSheetColumnNumber {
+    public int titleToNumber(String s) {
+        int base = 26;
+        int offset = 64;
+        int sum = 0;
+        int charValue = 0;
+        for (int i = 0; i < s.length(); i++) {
+           charValue = s.charAt(s.length()-i-1) - offset;
+           sum = (int) (sum + charValue * Math.pow(base, i));
+       }
+       return sum;
+    }
+}
