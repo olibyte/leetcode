9eb491c7c0fb373d70de9869ab9d8310f141e654
Oliver <ocben1@gmail.com>
2020-11-21 10:43:35 +1100

    ill stick with hashmap

--- interviewQuestions/romanToInt.java
+++ interviewQuestions/romanToInt.java
@@ -4,12 +4,8 @@ import java.util.Hashtable;
 class romanToInt {
     public int romanToInt(String s) {
         int result = 0;
-        //[0-9]
-        //[I,V,L,C,D,M]
-        //III => 'I','I','I' => { IV? => 5-1 = 4 : IX? => 10-1 = 9 : else I*1 = 3 }
         HashMap<Character, Integer> romanIntMap = new HashMap<>();
-        
-            //perform roman to int
+
         romanIntMap.put('I', 1);
         romanIntMap.put('V', 5);
         romanIntMap.put('X', 10);
@@ -17,6 +13,7 @@ class romanToInt {
         romanIntMap.put('C', 100);
         romanIntMap.put('D', 500);
         romanIntMap.put('M', 1000);
+
         int last = Integer.MAX_VALUE;
         for (char c : s.toCharArray()) {
             if(romanIntMap.get(c) > last) {
