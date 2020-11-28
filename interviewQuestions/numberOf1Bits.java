bfb7f76de55b22469de4d9f797bfda8882385cc2
Oliver <ocben1@gmail.com>
2020-11-28 19:28:00 +1100

    number of 1 bits cheeky version

---
+++ interviewQuestions/numberOf1Bits.java
@@ -0,0 +1,9 @@
+package interviewQuestions;
+
+public class numberOf1Bits {
+    public int hammingWeight (int n) {
+        Integer number = n;
+        return number.bitCount(n);
+        
+    }
+}
