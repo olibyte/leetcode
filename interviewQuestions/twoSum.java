4d04c231fe6a2d52b0789d80e1a0070a224e39e0
Oliver <ocben1@gmail.com>
2020-12-07 09:03:18 +1100

    twoSum

--- interviewQuestions/twoSum.java
+++ interviewQuestions/twoSum.java
@@ -8,7 +8,7 @@ public class twoSum {
         /**
          * //brute force for (int i = 0; i < nums.length; i++) { for (int j = i + 1; j <
          * nums.length; j++) { if ((nums[i] + nums[j]) == target) { result[0] = i;
-         * result[1] = j; } } }
+         * result[1] = j; } } return result; }
          */
         /**
          * //two pass hashmap HashMap<Integer,Integer> myMap = new HashMap<>(); for (int
