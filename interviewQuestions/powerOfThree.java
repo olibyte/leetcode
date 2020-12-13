6b7937dd78169282f55f8711741545f9b802e15a
Oliver <ocben1@gmail.com>
2020-12-14 08:57:12 +1100

    power of three solution

--- interviewQuestions/powerOfThree.java
+++ interviewQuestions/powerOfThree.java
@@ -2,6 +2,14 @@ package interviewQuestions;
 
 public class powerOfThree {
     public boolean isPowerOfThree(int n) {
-        
+        if (n == 0)
+            return false;
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+        //Integer limitation hack, 4 Byte integer's max positive value is 1162261467, therefore we can perform simple arithmetic:
+        // return n > 0 && 1162261467 % n == 0;
     }
+
 }
