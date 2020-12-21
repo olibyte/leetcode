7381aa22cb0fd9bdfbb059674d494bd847c05afa
Oliver <ocben1@gmail.com>
2020-12-21 21:48:09 +1100

    valid parentheses

--- interviewQuestions/validParentheses.java
+++ interviewQuestions/validParentheses.java
@@ -1,31 +1,46 @@
 package interviewQuestions;
 
+import java.util.HashMap;
 import java.util.Stack;
 
 public class validParentheses {
-    public boolean isValid(String s) {
-        //add parentheses to the stack
-        //if the stack is empty return true
-        // char[] myChars = s.toCharArray();
-        char[] open = new char[]{'[','{', '('};
-        char[] close = new char[]{']','}', ')'};
-        // Stack<Character> open = new  
-        Stack<Character> tagStack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == open[0]) {
-                if (s.)
-            }
-            if (s.charAt(i) == open[1]) {
+      // Hash table that takes care of the mappings.
+  private HashMap<Character, Character> mappings;
 
-            }
-            if (s.charAt(i) == open[2]) {
+  // Initialize hash map with mappings. This simply makes the code easier to read.
+  public validParentheses() {
+    this.mappings = new HashMap<Character, Character>();
+    this.mappings.put(')', '(');
+    this.mappings.put('}', '{');
+    this.mappings.put(']', '[');
+  }
 
-            }
-            if (tagStack.contains(s.charAt(i))) {
+  public boolean isValid(String s) {
 
-            }
-            tagStack.push(s.charAt(i));
+    // Initialize a stack to be used in the algorithm.
+    Stack<Character> stack = new Stack<Character>();
+
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+
+      // If the current character is a closing bracket.
+      if (this.mappings.containsKey(c)) {
+
+        // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
+        char topElement = stack.empty() ? '#' : stack.pop();
+
+        // If the mapping for this bracket doesn't match the stack's top element, return false.
+        if (topElement != this.mappings.get(c)) {
+          return false;
         }
-        return false;   
+      } else {
+        // If it was an opening bracket, push to the stack.
+        stack.push(c);
+      }
+    }
+
+    // If the stack still contains elements, then it is an invalid expression.
+    return stack.isEmpty();
+
     }
 }
