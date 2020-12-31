03453a21756432f9c5c81fbda619dcb6df9fa784
Oliver <ocben1@gmail.com>
2020-12-31 17:27:50 +1100

    reverse integer

---
+++ interviewQuestions/reverseInteger.java
@@ -0,0 +1,14 @@
+package interviewQuestions;
+
+public class reverseInteger {
+    public int reverse(int x) {
+        //cheeky version
+        String result = x < 0 ? new StringBuilder(String.valueOf(-x)).append("-").reverse().toString()
+        : new StringBuilder(String.valueOf(x)).reverse().toString();
+try {
+    return Integer.parseInt(result);
+} catch (Exception e) {
+    return 0;
+}
+    }
+}
