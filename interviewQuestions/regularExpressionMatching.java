f1c6a5347ef5c05fbfe23a723b2314174a799fc3
Oliver <ocben1@gmail.com>
2021-02-02 07:46:32 +1100

    regular expression matching

---
+++ interviewQuestions/regularExpressionMatching.java
@@ -0,0 +1,51 @@
+package interviewQuestions;
+
+class regularExpressionMatching {
+    public boolean isMatch(String s, String p) {
+        if (p.isEmpty()) return s.isEmpty();
+        boolean first_match = (!s.isEmpty() &&
+                               (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'));
+
+        if (p.length() >= 2 && p.charAt(1) == '*'){
+            return (isMatch(s, p.substring(2)) ||
+                    (first_match && isMatch(s.substring(1), p)));
+        } else {
+            return first_match && isMatch(s.substring(1), p.substring(1));
+        }
+    }
+}
+enum Result {
+    TRUE, FALSE
+}
+
+class dynamicRegularExpressionMatching {
+    Result[][] memo;
+
+    public boolean isMatch(String text, String pattern) {
+        memo = new Result[text.length() + 1][pattern.length() + 1];
+        return dp(0, 0, text, pattern);
+    }
+
+    public boolean dp(int i, int j, String text, String pattern) {
+        if (memo[i][j] != null) {
+            return memo[i][j] == Result.TRUE;
+        }
+        boolean ans;
+        if (j == pattern.length()){
+            ans = i == text.length();
+        } else{
+            boolean first_match = (i < text.length() &&
+                                   (pattern.charAt(j) == text.charAt(i) ||
+                                    pattern.charAt(j) == '.'));
+
+            if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
+                ans = (dp(i, j+2, text, pattern) ||
+                       first_match && dp(i+1, j, text, pattern));
+            } else {
+                ans = first_match && dp(i+1, j+1, text, pattern);
+            }
+        }
+        memo[i][j] = ans ? Result.TRUE : Result.FALSE;
+        return ans;
+    }
+}
