db2ac86c65ad24b1d7135f5c40358e168bae08b2
Oliver <ocben1@gmail.com>
2021-02-27 09:55:17 +1100

    merge intervals

--- interviewQuestions/jumpGame.java
+++ interviewQuestions/jumpGame.java
@@ -19,4 +19,16 @@ public class jumpGame {
             }
             return false;
     }
+    // greedy
+/* 
+    public boolean canJump(int[] nums) {
+            int lastPos = nums.length - 1;
+            for (int i = nums.length - 1; i >= 0; i--) {
+                if (i + nums[i] >= lastPos) {
+                    lastPos = i;
+                }
+            }
+            return lastPos == 0;
+        }
+        */
 }
