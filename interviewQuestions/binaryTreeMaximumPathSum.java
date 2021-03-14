4ef548cc4c65114551c67b482329cff1a2eea5f8
Oliver <ocben1@gmail.com>
2021-03-15 07:28:03 +1100

    binary tree maximum sum path

---
+++ interviewQuestions/binaryTreeMaximumPathSum.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class binaryTreeMaximumPathSum {
+    int result = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        maximumUtility(root);
+            return result;
+    }
+    public int maximumUtility(TreeNode root) {
+        if (root == null)
+            return 0;
+            //stores max path sum of left and right subtree
+        int left = maximumUtility(root.left);
+        int right = maximumUtility(root.right);
+        //case 1 current node lies in the path of maximumPathSum
+        int candidate1 = Math.max(Math.max(left,right) + root.val, root.val);
+        //case 2 current node is the root of maximumPathSum
+        int candidate2 = Math.max(candidate1, left+right+root.val);
+        //case 3 current node does not lie in maximumPathSum
+        result = Math.max(candidate2, result);
+
+        return candidate1;
+    }
+}
