b883b2f46186c477fff8a02858e0a0f0e3659ace
Oliver <ocben1@gmail.com>
2021-04-30 06:50:28 +1000

    perfect squares

--- interviewQuestions/perfectSquares.java
+++ interviewQuestions/perfectSquares.java
@@ -2,6 +2,15 @@ package interviewQuestions;
 
 class perfectSquares {
     public int numSquares(int n) {
-        
+        int memo[] = new int[n + 1];
+        memo[0] = 0;
+        memo[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++)
+                min = Math.min(1 + memo[i - (j * j)], min);
+            memo[i] = min;
+        }
+        return memo[n];
     }
 }
