48d91ef717881857fb62f02f57f424f2432c48b5
Oliver <ocben1@gmail.com>
2021-06-04 12:02:54 +1000

    max area of a piece of cake

--- june21/maximumAreaOfAPieceOfCake.java
+++ june21/maximumAreaOfAPieceOfCake.java
@@ -1,7 +1,23 @@
 package june21;
 
+import java.util.Arrays;
+
 public class maximumAreaOfAPieceOfCake {
     public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
-        return 0;
+        Arrays.sort(horizontalCuts);
+        Arrays.sort(verticalCuts);
+
+        int maxHorizontal = Math.max(horizontalCuts[0], h - horizontalCuts[horizontalCuts.length-1]);
+        int maxVertical = Math.max(verticalCuts[0], w - verticalCuts[verticalCuts.length-1]);
+        
+        for (int i = 1; i < horizontalCuts.length; i++) {
+            maxHorizontal = Math.max(maxHorizontal, horizontalCuts[i] - horizontalCuts[i-1]);
+        }
+            
+        for (int i = 1; i < verticalCuts.length; i++) {
+            maxVertical = Math.max(maxVertical, verticalCuts[i] - verticalCuts[i-1]);
+        }
+            
+        return (int)((long)maxHorizontal * maxVertical % 1000000007);   
     }
 }
