4ed3fb971edb6d240d9a47f98e0182feb7faf6a7
Oliver <ocben1@gmail.com>
2021-06-17 07:40:24 +1000

    generate parentheses

---
+++ june21/generateParentheses.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class generateParentheses {
+    private List<String> ans;
+    private int m;
+    
+    public List<String> generateParenthesis(int N) {
+        ans = new ArrayList<>();
+        m = 2 * N;
+        dfs(0, 0, 0);
+        return ans;
+    }
+    
+    private void dfs(int pos, int open, int seq) {
+        if (pos == m) {
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < m; i++)
+                res.append((seq & 1 << i) > 0 ? "(" : ")");
+            ans.add(res.toString());
+            return;
+        }
+        if (open > 0) dfs(pos+1, open-1, seq);
+        if (m - pos > open) dfs(pos+1, open+1, seq | 1 << pos);
+    }
+}
